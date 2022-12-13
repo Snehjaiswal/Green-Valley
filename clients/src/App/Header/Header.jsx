582e5cbf30e532223ce683186f8567b851733855
snehpnp <snehpnp@gmail.com>
2022-12-13 18:48:28 +0530

    heder hide

--- clients/src/App/Header/Header.jsx
+++ clients/src/App/Header/Header.jsx
@@ -15,49 +15,7 @@ function Header() {
   return (
     <>
       <div className="super_container bg-light header1">
-         <div className="top_bar">
-                <div className="container_fluid">
-                    <div className="row">
-                        <div className="col d-flex flex-row headermargin" >
-
-                            <div className="top_bar_content ms-auto" >
-                                <div className="top_bar_menu">
-                                    <ul className="standard_dropdown top_bar_dropdown">
-                                        <li>
-                                            <a href="#">
-                                                English
-                                                <i className="fas fa-chevron-down" />
-                                            </a>
-
-                                        </li>
-                                        <li>
-                                            <a href="#">
-                                                $ US dollar
-                                                <i className="fas fa-chevron-down" />
-                                            </a>
-
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="top_bar_user">
-                                    <div className="user_icon">
-                                        <img
-                                            src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918647/user.svg"
-                                            alt=""
-                                        />
-                                    </div>
-                                    <div>
-                                        <a href="#">Register</a>
-                                    </div>
-                                    <div>
-                                        <a href="#">Sign in</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>  
+    
 
         <div className="header_main">
           <div className="container">
