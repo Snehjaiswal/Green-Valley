9facc6a1997390651c855910ce0946d2854157c9
snehpnp <snehpnp@gmail.com>
2023-01-03 18:08:43 +0530

    okk

---
+++ Servers/app/models/Addproducts.js
@@ -0,0 +1,62 @@
+/*
+ * Title:   Login System Advanced 
+ * Author:     Sneh Jaiswal
+ * Created On: Fri Jun 17 2022 10:56:17 am
+ */
+
+"use strict"
+
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const LoginSchema = Schema({
+
+    Name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    Image: {
+        data: Buffer,
+        contentType: String
+    },
+    value: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String
+    },
+    colure: {
+        type: String,
+
+    },
+    description: {
+        type: String,
+
+    },
+    product_id: {
+        type: String,
+
+    },
+    isVerifyed: Boolean,
+
+},
+    {
+        timestamps: true
+    },
+
+
+)
+
+// collection creation 
+const LoginModel = model('LOGIN', LoginSchema, "User Login");
+
+
+
+
+
+module.exports = LoginModel;
+
